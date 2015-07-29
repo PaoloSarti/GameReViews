38571634fd4aeffc226b3169176f699f66d3ac2e
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2015-07-29 18:30:17 +0200

    GUI

---
+++ GameReViews/CustomListView.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GameReViews
+{
+    public partial class CustomListView : UserControl
+    {
+        public CustomListView()
+        {
+            InitializeComponent();
+
+            //Control[] controls = listView.Controls.Find("_dataGridView", true);
+            //DataGridView dgw = (DataGridView)controls[0];
+
+            _dataGridView.ColumnCount = 3;
+            _dataGridView.Name = "dataGridViewName";
+            _dataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            _dataGridView.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            //_dataGridView.CellBorderStyle = DataGridViewCellBorderStyle.None;
+
+            _dataGridView.Columns[0].Name = "Nome Gioco";
+            _dataGridView.Columns[1].Name = "Nome Recensore";
+            _dataGridView.Columns[2].Name = "Valutazione";
+
+            _dataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            string[] row01 = { "Nome Gioco", "Nome recensore", "Valutazione" };
+            string[] row11 = { "Nome Gioco", "Nome recensore", "Valutazione" };
+            string[] row21 = { "Nome Gioco", "Nome recensore", "Valutazione" };
+            string[] row31 = { "Nome Gioco", "Nome recensore", "Valutazione" };
+            string[] row41 = { "Nome Gioco", "Nome recensore", "Valutazione" };
+            string[] row51 = { "Nome Gioco", "Nome recensore", "Valutazione" };
+            string[] row61 = { "Nome Gioco", "Nome recensore", "Valutazione" };
+
+            _dataGridView.Rows.Add(row01);
+            _dataGridView.Rows.Add(row11);
+            _dataGridView.Rows.Add(row21);
+            _dataGridView.Rows.Add(row31);
+            _dataGridView.Rows.Add(row41);
+            _dataGridView.Rows.Add(row51);
+            _dataGridView.Rows.Add(row61);
+
+            _dataGridView.Columns[0].DisplayIndex = 0;
+            _dataGridView.Dock = DockStyle.Fill;
+        }
+
+        private void _dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GameReViews/UserProfileView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GameReViews
+{
+    public partial class UserProfileView : UserControl
+    {
+        public UserProfileView()
+        {
+            InitializeComponent();
+        }
+
+        private void _userInfoContainer_root_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void _aggiungiPreferenzaButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
